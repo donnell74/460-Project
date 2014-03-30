9573b872d0f0cbffb3efe627f2adc97832bf5a16
Gregory Donnell <gregory.donnell74@gmail.com>
2014-03-29 20:37:16 -0500

    Fixed seg fault on no port given to server

--- Server/game.cpp
+++ Server/game.cpp
@@ -129,7 +129,8 @@ int main(int argc, char* argv[])
 
   if ( argc == 1 || argc > 3 )
   {
-   my_server->die("Usage: ./server <port>");
+	cerr << "Usage: ./server <port>" << endl;
+        exit(EXIT_SUCCESS); 
   }
 
   else
