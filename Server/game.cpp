98f126c4f252571c1e7b27eb97711bc0c43ac967
Matthew Duff <md@grok.MissouriState.edu>
2014-04-22 12:16:26 -0500

    Added call to update scores from server

--- Server/game.cpp
+++ Server/game.cpp
@@ -45,138 +45,129 @@ void display_options( )
 }
 */
 
+
+//|handle_input
 void handle_input()
 {
-  char type = 0;
-  string inp = {};
+    char type = 0;
+    string inp = {};
 
-  cin >> inp;
-  type = inp[0];
-  cout << type << endl;
-  switch( toupper( type ) )
-  {
+    cin >> inp;
+    type = inp[0];
+    cout << type << endl;
+    switch( toupper( type ) )
+    {
   
-    case 'B':
-      my_server->send_playing_cards( std_indexes );
-      my_server->update_scores();
-      display_sets( my_server->playing_deck->get_cards() );
-      break;
+        case 'B':
+            my_server->send_playing_cards( std_indexes );
+            my_server->update_scores();
+            display_sets( my_server->playing_deck->get_cards() );
+            break;
 
-    case 'M':
-      display_options();
-      break;
+        case 'M':
+            display_options();
+            break;
 
-    case 'Q':
-      {
-        my_server->~Server();
-      }
-      break;
+        case 'Q':
+            my_server->~Server();
+            break;
 
-    case 'D':
-      {
-        my_server->deck->display( 0 );
-      }
-      break;
+        case 'D':
+            my_server->deck->display( 0 );
+            break;
       
-    case 'S':
-      {
-        cout<<"shuffling..."<<endl;
-        my_server->deck->shuffle();
-      }
-      break;
+        case 'S':
+            cout << "Shuffling..." << endl;
+            my_server->deck->shuffle();
+            break;
 
-    case 'A':
-      {
-        my_server->deck->mem_display();
-      }
-      break;
+        case 'A':
+            my_server->deck->mem_display();
+            break;
     
-    case 'W':
-      {
-	display_sets( my_server->playing_deck->get_cards() );
-      }
-      break;
+        case 'W':
+	    display_sets( my_server->playing_deck->get_cards() );
+            break;
       
-  case 'P':
-    {
-      my_server->playing_deck->display( 1 );
-    }
-    break;
+        case 'P':
+            my_server->playing_deck->display( 1 );
+            break;
 
-  case 'U':
-    {
-      my_server->update_scores();
-      cout << "Updating" << endl;
-    }
+        case 'U':
+           my_server->update_scores();
+           cout << "Updating..." << endl;
   
-  default:
-    break;
-  }
-}
-
-void *wait_for_client_wrap ( void *arg )
-{
-  my_server->wait_for_client();
-
-  return arg;
-}
-
-
-void sig_wrap_cleanup ( int sig )
-{
-  my_server->~Server();
-}
-
-
-int main(int argc, char* argv[])
-{
-  char LOCALHOST[] = "127.0.0.1";
-  int DELAY = 15;
-
-  if ( argc == 1 || argc > 3 )
-  {
-	cerr << "Usage: ./server <port>" << endl;
-        exit(EXIT_SUCCESS); 
-  }
-
-  else
-  {
-    if ( argc == 2 ) // No Addr or Dealy given
-    {
-      my_server = new Server( atoi( argv[1] ), LOCALHOST, DELAY );
+        default:
+          break;
     }
+}
 
-    if ( argc == 3 ) // Addr and Port Given, but no Dealy
+
+//|*wait_for_client_wrap
+void *wait_for_client_wrap( void *arg )
+{
+    my_server->wait_for_client();
+    return arg;
+}
+
+
+//|sei_wrap_cleanup
+void sig_wrap_cleanup( int sig )
+{
+    my_server->~Server();
+}
+
+
+//|main
+int main( int argc, char* argv[] )
+{
+    char LOCALHOST[] = "127.0.0.1";
+    int DELAY = 15;
+
+    if ( argc == 1 || argc > 3 )
     {
-      my_server = new Server( atoi( argv[1] ), LOCALHOST, atoi(argv[2]) );
-    }
+        cerr << "Usage: ./server <port>" << endl;
+        exit( EXIT_SUCCESS ); 
+    }  
+
+    else
+    {
+        if ( argc == 2 ) // No Addr or Dealy given
+        {
+            my_server = new Server( atoi( argv[1] ), LOCALHOST, DELAY );
+        }  
+
+        if ( argc == 3 ) // Addr and Port Given, but no Dealy
+        {
+          my_server = new Server( atoi( argv[1] ), LOCALHOST, atoi(argv[2]) );
+        }
     
-  }
+    }
 
-  //Initialize deck and playing deck
-  my_server->deck = new Deck();
-  my_server->playing_deck = new Deck( 1 );
+    //Initialize deck and playing deck
+    my_server->deck = new Deck();
+    my_server->playing_deck = new Deck( 1 );
 
-  //Shuffle deck
-  my_server->deck->shuffle();
-  my_server->deck->shuffle();
+    //Shuffle deck
+    my_server->deck->shuffle();
+    my_server->deck->shuffle();
 
-  // bind TERM to cleanup
-  struct sigaction action = {};
+    // bind TERM to cleanup
+    struct sigaction action = {};
 
-  action.sa_handler = sig_wrap_cleanup;
-  sigaction( SIGINT, &action, nullptr );
+    action.sa_handler = sig_wrap_cleanup;
+    sigaction( SIGINT, &action, nullptr );
   
-  pthread_t thread;
-  if ( pthread_create( &thread, NULL, wait_for_client_wrap, nullptr ) != 0 )
-  {
-    my_server->die( "Couldn't start wait for client" );
-  }
+    pthread_t thread;
+    if ( pthread_create( &thread, NULL, wait_for_client_wrap, nullptr ) != 0 )
+    {
+        my_server->die( "Couldn't start wait for client" );
+    }  
 
-  pthread_detach( thread );
+    pthread_detach( thread );
 
-  my_server->wait_for_input();
+    my_server->wait_for_input();
   
-  my_server->cleanup();
+    my_server->cleanup();
 }
 
